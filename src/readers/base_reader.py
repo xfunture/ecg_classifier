a3a1eaeef22a5c78ccce18aef1c20a51319c8c66
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2018-05-05 10:14:04 +0300

    Implement CSVReader

--- src/readers/base_reader.py
+++ src/readers/base_reader.py
@@ -1,6 +1,17 @@
 class BaseReader:
-    def read_ecg_file(self, file_path):
-        raise Exception('Method is not implemented')
+    def read_ecg_file(self, signal_file_path):
+        return self.read_signal_from_file(signal_file_path)
 
     def read_ecg_data(self, records_filename, labels_filename, signals_directory):
+        signals = self.read_signals_from_file(records_filename, signals_directory)
+        labels = self.read_labels_from_file(labels_filename)
+        return signals, labels
+
+    def read_signals_from_file(self, records_filename, signals_directory):
+        raise Exception('Method is not implemented')
+
+    def read_signal_from_file(self, signal_file_path):
+        raise Exception('Method is not implemented')
+
+    def read_labels_from_file(self, labels_filename):
         raise Exception('Method is not implemented')
--- src/readers/csv_reader.py
+++ src/readers/csv_reader.py
@@ -1,22 +1,21 @@
 from src.readers.base_reader import BaseReader
 import pandas as pd
-import wfdb
 import keras
 
 
-class PhysionetReader(BaseReader):
+class CSVReader(BaseReader):
     def read_signals_from_file(self, records_filename, signals_directory):
         signals = []
         records_file = open(records_filename, 'r')
         for record_name in records_file:
-            signal_file_path = '{}/{}'.format(signals_directory, record_name.strip())
+            signal_file_path = '{}/{}.csv'.format(signals_directory, record_name.strip())
             digital_signal = self.read_signal_from_file(signal_file_path)
             signals.append(digital_signal)
         return signals
 
     def read_signal_from_file(self, signal_file_path):
-        record = wfdb.rdsamp(signal_file_path)
-        digital_signal = record.adc()[:, 0]
+        record = pd.read_csv(signal_file_path, header=None, names=['signal'])
+        digital_signal = record['signal'].as_matrix()
         return digital_signal
 
     def read_labels_from_file(self, labels_filename):
@@ -30,4 +29,4 @@ class PhysionetReader(BaseReader):
         return labels
 
 
-physionet_reader = PhysionetReader()
+csv_reader = CSVReader()
