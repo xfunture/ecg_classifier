148f3d37670b5110aeb83c918892aa38bfe41708
rick <15768184216@163.com>
2018-10-01 13:27:33 +0800

    second

--- src/utils/persistance.py
+++ src/utils/persistance.py
@@ -1,6 +1,6 @@
 import pickle
 from sklearn.metrics import confusion_matrix
-
+import os
 
 def save_processed_data(file_path, data):
     with open(file_path, 'wb') as file:
@@ -14,6 +14,9 @@ def load_processed_data(file_path):
 
 
 def save_results(file_path, model_name, epochs, y_actual, y_predict):
+    path = os.path.dirname(file_path)
+    if os.path.exists(path) == False:
+        os.mkdir(path)
     with open(file_path, 'w') as file:
         file.write('Model: {}\nEpochs: {}\nConfusion Matrix:\n{}'
                    .format(model_name, epochs, confusion_matrix(y_actual, y_predict)))
